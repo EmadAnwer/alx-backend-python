2f3280a2ce331421f95363b46680977f4db39e28
Emad Anwer <emadanwer.official@gmail.com>
2024-01-11 14:19:16 +0200

    [add] task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 5-sum_list.py """
+
+import functools
+
+
+def sum_list(input_list: [float]) -> float:
+    """sum_list"""
+
+    return functools.reduce(lambda a, b: a + b, input_list)
