e1aedd92ac2a4510c13ea3c0b002ea60a99b9210
Emad Anwer <emadanwer.official@gmail.com>
2024-01-11 14:22:50 +0200

    [fix] task 5 use sum

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """ 5-sum_list.py """
 
-import functools
-
 
 def sum_list(input_list: list[float]) -> float:
     """sum_list"""
 
-    return functools.reduce(lambda a, b: a + b, input_list)
+    return sum(input_list)
