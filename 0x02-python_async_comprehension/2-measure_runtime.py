309fc72e82d0e970b43f79be2504e387bc3ce525
EmadAnwer <emadanwer.official@gmail.com>
2024-01-16 12:13:03 +0200

    [fix] task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,8 +9,7 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 async def measure_runtime() -> float:
     """measure_runtime"""
-    start: float = time.perf_counter()
+    start: float = time.time()
     await async_comprehension()
-    end: float = time.perf_counter()
-
+    end: float = time.time()
     return end - start
